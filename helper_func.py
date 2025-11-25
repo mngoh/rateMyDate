4bfd93e89b6321d5de3f9fce76a8ce0dcd6d97cb
mngoh <mngoh8@gmail.com>
2025-11-24 20:58:49 -0500

    updates

--- helper_func.py
+++ helper_func.py
@@ -4,8 +4,8 @@ def get_recommendation(score):
     date_outcomes = {
             (-12,0): "This was a Horrible date: RUN",
             (0,4): "This was a poor date, reach out as soon as possible to express your feelings and move on.",
-            (4, 8): "This was an Above Average Date: Consider a second date if you feel good.",
-            (8, 12): "This was a GREAT DATE: Decide in the next day how you'd like to set up the second date."
+            (4, 8): "This was an Above Average Date: Consider a second date after reflecting.",
+            (8, 12): "This was a GREAT DATE: Decide in the next day how you'd like to proceed."
         }
     for (lower, upper), message in date_outcomes.items():
         if lower < score <= upper:  
